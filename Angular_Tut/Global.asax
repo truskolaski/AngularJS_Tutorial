447ad6efe5e3189ac5df6ed1387e022b8ac85201
Krzysztof Truskolaski <truskolaski.krzysztof@gmail.com>
2017-04-18 16:28:00 +0200

    dev

---
+++ Angular_Tut/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Angular_Tut.MvcApplication" Language="C#" %>
---
+++ Angular_Tut/Gruntfile.js
@@ -0,0 +1,24 @@
+﻿module.exports = function (grunt) {
+    // load Grunt plugins from NPM
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // configure plugins
+    grunt.initConfig({
+        uglify: {
+            out: {
+                files: { 'app/Scripts/Mins/app.min.js': ['app/Scripts/app.js'] }
+            },
+        },
+
+        watch: {
+            scripts: {
+                files: ['app/Scripts/**/*.js', "app/Scripts/app.js"],
+                tasks: ['uglify']
+            } 
+        }
+    });
+
+    // define tasks
+    grunt.registerTask('default', ['uglify', 'watch']);
+};
